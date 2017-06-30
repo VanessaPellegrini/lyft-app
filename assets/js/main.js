9427af3e2678f2d7312d39c2e8aa316e84fa24c8
VannPellegrini <vanessapellegrini@hotmail.com>
2017-06-30 16:20:54 -0400

    validacion telefono prueba

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,27 @@
-main.js
+/*sign up telefono 1 validacion*/
+$("#submitbutton").click(function(){
+	var errorMessage = "";
+
+	var fieldsMissing = "";
+
+	if ($("#phone").val() == "") {
+
+		fieldsMissing += "<br>Telephone";
+	}
+
+	if (fieldsMissing != "") {
+		errorMessage += "<p>The following field(s) are missing" + fieldsMissing;
+	}	
+
+	if ($.isNumeric($("#phone").val()) == false) {
+		errorMessage += "<p> Your phone Number is not valid</p>";
+	}
+
+	if (errorMessage != "") {
+		$("#errorMessage").html(errorMessage);
+	} else {
+		$("#successMessage").show();
+		$("#errorMessage").hide();
+	}
+
+});
