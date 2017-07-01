d1990813ef5d4b4a183a25615eeb97e678a63e2d
vanita22 <vanitayrafita2004@gmail.com>
2017-07-01 19:04:34 -0300

    subiendo nuevo index de codigo por vanessa perez

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,44 @@
 /*alert("hola");*/
 /*sign up telefono 1 validacion*/
-$("#submitButton").click(function(){
-	var errorMessage = "";
-	var fieldsMissing = "";
+$(document).ready(function(){
 
-	if ($("#telefono").val().length != 9) {
+	$("#submitButton").click(function(){
+		var errorMessage = "";
+		var fieldsMissing = "";
 
-		errorMessage += "<p> Your phone Number is not valid</p>";
-	}
+		if ($("#telefono").val().length != 9) {
 
-	if ($("#telefono").val() == "") {
+			errorMessage += "<p> Your phone Number is not valid</p>";
+		}
 
-		fieldsMissing += "<br>Telefono";
-	}
+		if ($("#telefono").val() == "") {
+
+			fieldsMissing += "<br>Telefono";
+		}
 	
-	if (fieldsMissing != "") {
-		errorMessage += "<p>The following field(s) are missing" + fieldsMissing;
-	}	
+		if (fieldsMissing != "") {
+			errorMessage += "<p>The following field(s) are missing" + fieldsMissing;
+		}	
 
-	if ($.isNumeric($("#telefono").val()) == false) {
-		errorMessage += "<p> Your phone Number is not valid</p>";
-	}
+		if ($.isNumeric($("#telefono").val()) == false) {
+			errorMessage += "<p> Your phone Number is not valid</p>";
+		}
 
-	if (errorMessage != "") {
-		$("#errorMessage").html(errorMessage);
-	} else {
-		alert("Your code to enter is:" + " LAB-" + Math.floor(Math.random() * 10) + Math.floor(Math.random() * 10) + Math.floor(Math.random() * 10));
-		$("#successMessage").show();
-		$("#errorMessage").hide();
-	}
+		if (errorMessage != "") {
+			$("#errorMessage").html(errorMessage);
+		} else {
+			
+			alert("Your code to enter is:" + " LAB-" + Math.floor(Math.random() * 10) + Math.floor(Math.random() * 10) + Math.floor(Math.random() * 10));
+			$("#successMessage").show();
+			$("#errorMessage").hide();
+		}
+	})
 
+	$(".button-1").click(function(){
+		if("#validador" == " "){
+			alert("Ingrese código");
+		}
+	})
 });
+
+
