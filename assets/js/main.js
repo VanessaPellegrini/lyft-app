515fd3450ec0a70faac34b93d456bf5226379109
Fa Diaz <diazgonzalez.perla@gmail.com>
2017-07-02 01:33:45 -0400

    mapa listo

--- assets/js/main.js
+++ assets/js/main.js
@@ -95,7 +95,7 @@ $(document).ready(function () {
 	/*validar email y nombre*/
 	$("#submitBtnEmail").click(function(e){
 
-  		function isChecked{
+  		/*function isChecked{
   			$(document).on("change", ".checkbox", function(){
 		  		if (this.checked) {
 		  			return true;
@@ -104,6 +104,7 @@ $(document).ready(function () {
 		  		}
   			});
   		};
+  	};*/
 
 		function isEmail(email) {
   		var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
@@ -156,3 +157,42 @@ $(document).ready(function () {
 		}
 	});
 });
+/*Mapa*/
+
+function initMap(){
+	var map = new google.maps.Map(document.getElementById("map"),{
+		zoom: 5,
+		center: {lat: -9.1191427, lng: -77.0349046},
+		mapTypeControl: false,
+		zoomControl: false,
+		streetViewControl: false
+	});
+	function buscar(){
+		if(navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
+		}
+	}
+	var latitud,longitud;
+
+	var funcionExito = function(posicion){
+		latitud = posicion.coords.latitude;
+		longitud = posicion.coords.longitude;
+		var miUbicacion = new google.maps.Marker({
+			position: {lat:latitud, lng:longitud},
+			animation: google.maps.Animation.DROP,
+			map: map, 
+			icon: "assets/img/facebook-placeholder.png"
+		});
+		map.setZoom(17);
+		map.setCenter({lat:latitud, lng:longitud});
+	}
+	var funcionError = function(error){
+		alert("Tenemos un problema para encontrar tu ubicación");
+	}
+	function initialize() {//autocompletar direcciones
+		var inputDestino = document.getElementById('destino');
+		var autocompletados = new google.maps.places.Autocomplete(inputDestino);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+buscar();
+}
