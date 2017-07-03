a7b234a754e75f963df729c25aeeb661ca823449
Fa Diaz <diazgonzalez.perla@gmail.com>
2017-07-02 23:39:15 -0400

    js mapa

--- assets/js/main.js
+++ assets/js/main.js
@@ -157,3 +157,38 @@ $(document).ready(function () {
 		}
 	});
 });
+/*Mapa*/
+var map;
+var miUbicacion;
+var latitud, longitud;
+
+document.getElementById("encuentrame").addEventListener("click", initMap);
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 5,
+        center: { lat: -9.1191427, lng: -77.0349046 },
+        mapTypeControl: false,
+        zoomControl: false,
+        streetViewControl: false
+    });
+    function buscar() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
+        }
+    }   
+
+    var funcionExito = function (posicion) {
+        latitud = posicion.coords.latitude;
+        longitud = posicion.coords.longitude;
+        miUbicacion = new google.maps.Marker({
+            position: { lat: latitud, lng: longitud },
+            animation: google.maps.Animation.BOUNCE,
+            map: map,
+            icon: "assets/img/circulo.png"
+        });
+        map.setZoom(17);
+        map.setCenter({ lat: latitud, lng: longitud });
+    }
+    var funcionError = function (error) {
+        alert("Tenemos un problema para encontrar tu ubicación");
+    }
