f4e3c7ed844205c28d41b8595cf3183fb21dfb01
VanessaPellegrini <vanessapellegrini@hotmail.com>
2017-07-04 00:25:23 -0400

    Update checkbox.js

--- assets/js/checkbox.js
+++ assets/js/checkbox.js
@@ -1,4 +1,54 @@
 $(document).ready(function(){
+	/*validar email y nombre*/
+	$("#submitBtnEmail").click(function(e){
+
+		function isEmail(email) {
+  		var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+ 		 return regex.test(email);
+		};
+
+		function isName(value, element){
+			var regex = /^[a-zA-Z," "]+$/;
+			return regex.test(value);
+		};
+
+		var errorMessage = "";
+		var fieldsMissing = "";
+
+		
+		if ($("#email").val() == "") {
+
+			fieldsMissing += "<br>Email";
+		}
+		
+		if ($("#name").val() == "") {
+
+			fieldsMissing += "<br>Code";
+		}
+
+		if (fieldsMissing != "") {
+			errorMessage += "<p>The following field(s) are missing" + fieldsMissing;
+		}
+
+		if (isEmail($("#email").val()) == false ) {
+			e.preventDefault();
+			errorMessage += "<p>Your Email address is not valid</p>";
+		}
+
+		if (isName($("#name").val()) == false ) {
+			e.preventDefault();
+			errorMessage += "<p>Your name and lastname are not valid</p>";
+		}
+
+		if (errorMessage != "") {
+			$("#errorMessage").html(errorMessage);
+		} else {
+			$("#errorMessage").hide();
+			e.preventDefault();
+			window.location = "map_4.html";
+		}
+	});
+	
 	var rules={'q2[]':{required:true,'minlength':1}}
 	$("#form1").validate({'rules':rules});
 		
